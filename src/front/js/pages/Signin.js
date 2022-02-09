b2c61bc3011d298f23336e8ee3d68c80e00414cf
Mazen Naeim <naeimmazen@gmail.com>
2022-02-09 04:42:51 +0000

    login page

--- src/front/js/pages/Signin.js
+++ src/front/js/pages/Signin.js
@@ -23,20 +23,40 @@ function Signin() {
       {token && token != "" && token != undefined ? (
         "You are logged in with this token" + token
       ) : (
-        <div>
-          <input
-            type="text"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button onClick={handleClick}>login</button>
+        <div className="signinScreen">
+          <div className="signinScreen_background">
+            <img className="signinScreen_logo" src={AMTV1} alt="main-logo" />
+            <div className="signinScreen_gradient" />
+          </div>
+          <div className="signinScreen_body">
+            <>
+              <div className="signinScreen_input">
+                <form>
+                  <h1>Sign In</h1>
+                  <input
+                    placeholder="Username"
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <input
+                    placeholder="Password"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <Button onClick={handleClick}>Watch Now</Button>
+                  <br></br>
+                  <h5>
+                    <span className="signup_gray">New to AM-TV? </span>
+                    <Link to="/register">
+                      <span className="signup_link">Sign Up now.</span>
+                    </Link>
+                  </h5>
+                </form>
+              </div>
+            </>
+          </div>
         </div>
       )}
     </div>
